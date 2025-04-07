e1f9dbbd95440520a26179091d8176f8aba0cc8c
Stefano Sguotti <58917457+pta19059@users.noreply.github.com>
2025-04-07 11:20:12 +0100

    Create sidebars.js

---
+++ Labs/sidebars.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tutorialSidebar: [
+    {
+      type: 'doc',
+      id: 'intro', // Matches the ID defined in `Intro.md`
+      label: 'Introduction', // Label for the sidebar
+    },
+    // Puoi aggiungere altre voci alla sidebar qui
+  ],
+};
